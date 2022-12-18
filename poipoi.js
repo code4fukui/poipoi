f600706de5a773787324719da938b9baf9f5532d
Taisuke Fukuno <fukuno@jig.jp>
2022-12-18 10:27:41 +0900

    分別画面追加

--- poipoi.js
+++ poipoi.js
@@ -1,5 +1,11 @@
 import { serveAPI } from "https://js.sabae.cc/wsutil.js";
+import { CSV } from "https://js.sabae.cc/CSV.js";
 
-serveAPI("/api", async (param) => {
+serveAPI("/api", async (param, req, path, conninfo) => {
+  if (path == "/api/separation") {
+    const url = "https://data.odp.jig.jp/viewcsv/jp/fukui/sabae/203.csv";
+    const data = await CSV.fetchJSON(url);
+    return data;
+  }
   return "poipoi";
 });
---
+++ static/separate.js
@@ -0,0 +1,35 @@
+import { fetchJSON } from "https://js.sabae.cc/fetchJSON.js";
+import { ArrayUtil } from "https://js.sabae.cc/ArrayUtil.js";
+
+export const showSeparation = async (main) => {
+  main.innerHTML = "";
+  const h1 = document.createElement("h1");
+  h1.textContent = "家庭ごみの分別";
+  main.appendChild(h1);
+
+  const list = document.createElement("dl");
+  main.appendChild(list);
+
+  const data = await fetchJSON("/api/separation");
+
+  const cates = ArrayUtil.toUnique(data.map(d => d.分別区分));
+  for (const cate of cates) {
+    const dt = document.createElement("dt");
+    dt.textContent = cate;
+    list.appendChild(dt);
+    dt.onclick = () => {
+      if (dt.children.length == 0) {
+        const ch = data.filter(d => d.分別区分 == cate).map(d => d.名称);
+        for (const c of ch) {
+          const dd = document.createElement("dd");
+          dd.textContent = c;
+          dt.appendChild(dd);
+        }
+      } else {
+        while (dt.children.length > 0) {
+          dt.children[0].remove();
+        }
+      }
+    };
+  }
+};
